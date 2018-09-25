1e970a74a7c00bf5a7f2ded1b52bef5b85eefbf4
Rohan Prasad <thewarlock321@gmail.com>
2018-09-26 00:20:15 +0530

    Simplify database

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import pyrebase
 import datetime
 import json
+import uuid
 
 with open('credentials/firebase.json') as f:
     auth = json.load(f)
@@ -9,9 +10,9 @@ with open('credentials/firebase.json') as f:
 
 config = {
     "apiKey": auth["private_key"],
-    "authDomain": auth["project_id"]+".firebaseapp.com",
+    "authDomain": auth["project_id"] + ".firebaseapp.com",
     "databaseURL": "https://" + auth["project_id"] + ".firebaseio.com/",
-    "storageBucket": auth["project_id"]+ ".appspot.com",
+    "storageBucket": auth["project_id"] + ".appspot.com",
     "serviceAccount": "credentials/firebase.json"}
 
 firebase = pyrebase.initialize_app(config)
@@ -27,9 +28,9 @@ def healthCheck():
 @app.route('/dialogflow', methods=['POST'])
 def firebase_fulfillment():
     req = request.json
-    print (req)
+    print(req)
     action = req["queryResult"]["action"]
-    print (action)
+    print(action)
     if action == "save_complaint":
         response = create_complaint(req)
     elif action == "initiate_call":
@@ -40,7 +41,7 @@ def firebase_fulfillment():
         response = create_call_complaint(req)
     else:
         response = {
-        "fulfillmentText": "Something went wrong. Please try again later."
+            "fulfillmentText": "Something went wrong. Please try again later."
         }
     return jsonify(response)
 
@@ -51,11 +52,11 @@ def save_mobile(data):
     mobile = data["queryResult"]["parameters"]["phone_number"]
     db.child("users").child(firebase_uid).child("Mobile Number").set(str(mobile))
     response = {
-    "fulfillmentText": "I've saved your phone number.",
-    "followupEventInput": {
-    "name": "continue_call_details",
-    "languageCode": "en-US"
-    }
+        "fulfillmentText": "I've saved your phone number.",
+        "followupEventInput": {
+            "name": "continue_call_details",
+            "languageCode": "en-US"
+        }
     }
     return response
 
@@ -77,8 +78,8 @@ def create_call_complaint(data):
     free_time = dict({})
 
     free_time = {
-    "Time": raw_params["time"],
-    "Date": raw_params["date"]
+        "Time": raw_params["time"],
+        "Date": raw_params["date"]
     }
     complaint_params = {
         "Product Type": raw_params["product_type"],
@@ -90,52 +91,51 @@ def create_call_complaint(data):
         "Date": date,
         "Time Slot Chosen": "0",
         "Time Slots": {"Slot 1": {"Time": "0", "Date": "0"},
-            "Slot 2": {"Time": "0", "Date": "0"},
-            "Slot 3": {"Time": "0", "Date": "0"}},
+                       "Slot 2": {"Time": "0", "Date": "0"},
+                       "Slot 3": {"Time": "0", "Date": "0"}},
         "Progress": "Under review.",
         "Free Time": free_time,
         "Details of Call": {
-        "Time": "0",
-        "Date": "0"}
-        }
+            "Time": "0",
+            "Date": "0"}
+    }
+
+    complaint_id = str(uuid.uuid4()[:8])
     db = firebase.database()
-    firebase_response = db.child(
+    db.child(
         'user_data').child(
         firebase_uid).child(
-        'Complaints').push(complaint_params)
+        'Complaints').child(complaint_id).set(complaint_params)
 
     fulfillment_response = {
         "fulfillmentText":
-        "You appointment was successfully registered. The reference number for this complaint is " + firebase_response["name"]}
+            "You appointment was successfully registered. The reference number for this complaint is " + complaint_id}
     return fulfillment_response
-
 
 
 def check_mobile(data):
     firebase_uid = data['session'].split('/')[-1]
     db = firebase.database()
-    mobile = db.child("users").child(firebase_uid).child("Mobile Number").get().val()
-    if mobile == "0" or mobile == None:
+    mobile = db.child("user_data").child(firebase_uid).child("Mobile Number").get().val()
+    if mobile == "0" or mobile is None:
         response = {
-        "followupEventInput": {
-        "name": "request_mobile",
-        "languageCode": "en-US"
-        }}
+            "followupEventInput": {
+                "name": "request_mobile",
+                "languageCode": "en-US"
+            }}
     else:
-        print (mobile)
+        print(mobile)
         response = {
-        "followupEventInput": {
-        "name": "continue_call_details",
-        "languageCode": "en-US"
-        }
+            "followupEventInput": {
+                "name": "continue_call_details",
+                "languageCode": "en-US"
+            }
         }
     return response
 
 
-
 @app.route('/getcomplaint/', methods=['GET'])
 def return_complaints():
-
     user_id = request.args.get('firebase_uid')
     db = firebase.database()
     data = db.child("user_data").child(user_id).child("Complaints").get().val()
@@ -143,11 +143,10 @@ def return_complaints():
 
 
 def create_complaint(data):
-
     firebase_uid = data['session'].split('/')[-1]
     contexts = data['queryResult']['outputContexts']
     for i in contexts:
-        if ('globals' in i['name']):
+        if 'globals' in i['name']:
             context = i
             break
 
@@ -166,33 +165,33 @@ def create_complaint(data):
         "Date": date,
         "Time Slot Chosen": "0",
         "Time Slots": {"Slot 1": {"Time": "0", "Date": "0"},
-            "Slot 2": {"Time": "0", "Date": "0"},
-            "Slot 3": {"Time": "0", "Date": "0"}},
+                       "Slot 2": {"Time": "0", "Date": "0"},
+                       "Slot 3": {"Time": "0", "Date": "0"}},
         "Progress": "Under review.",
         "Free Time": {
-        "Date": ["0"],
-        "Time": ["0"],
+            "Date": ["0"],
+            "Time": ["0"],
         },
         "Details of Call": {
-        "Time": "0",
-        "Date": "0"}
-        }
+            "Time": "0",
+            "Date": "0"}
+    }
 
+    complaint_uid = str(uuid.uuid4())[:8]
     db = firebase.database()
-    firebase_response = db.child(
+    db.child(
         'user_data').child(
         firebase_uid).child(
-        'Complaints').push(complaint_params)
+        'Complaints').child(complaint_uid).set(complaint_params)
 
     fulfillment_response = {
         "fulfillmentText":
-        "You complaint was successfully registered. The reference number for this complaint is " + firebase_response["name"]}
+            "You complaint was successfully registered. The reference number for this complaint is " + complaint_uid}
     return fulfillment_response
 
 
 @app.route('/choosetimeslot/', methods=["POST"])
 def chooseTimeSlot():
-
     req = request.json
     firebase_uid = req["firebase_uid"]
     complaint_id = req["complaint_id"]
@@ -202,7 +201,7 @@ def chooseTimeSlot():
         "Complaints").child(
         complaint_id).update({
         "Time Slot Chosen": req["time_slot"]
-        })
+    })
     return jsonify({"Status": "200", "Message": "successfully chosen time"})
 
 
